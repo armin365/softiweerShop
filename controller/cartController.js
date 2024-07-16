ebdc801a9cfdf692dacc91bb518870374612653c
armin365 <mohamedamiinabdi12@gmail.com>
2024-07-16 11:38:05 +0300

    delete cart item and wishlisht item added

--- controller/cartController.js
+++ controller/cartController.js
@@ -70,3 +70,59 @@ export const addToWishlist = async(req,res)=>{
         res.status(500).json({ Error: e.message })
     }
 }
+
+export const removecartItem = async(req,res)=>{
+  try {
+      const{userId, productId}= req.body;
+    const product = await ProductModel.findById(productId);
+    let user = await UserModel.findById(userId).populate("cart.product").populate("wishlist.product");
+
+    for (let index = 0; index < user.cart.length; index++) {
+      if (user.cart.product._id.equals(product._id)) {
+        if (user.cart[index].quantity == 1) {
+          user.cart.splice(index,1)
+        }else{
+          user.cart[index].quantity--;
+        }
+      }
+      
+    }
+    user = await user.save();
+    res.status(200).json(user)
+
+  } catch (e) {
+      res.status(500).json({ Error: e.message })
+  }
+}
+
+export const deletecartItem = async(req,res)=>{
+  try {
+      const{userId, index}= req.body;
+
+    let user = await UserModel.findById(userId).populate("cart.product").populate("wishlist.product");
+   if (user) {
+    user.cart.splice(index,1);
+   }
+    user = await user.save();
+    res.status(200).json(user)
+
+  } catch (e) {
+      res.status(500).json({ Error: e.message })
+  }
+}
+
+export const deleteWishlisttItem = async(req,res)=>{
+  try {
+      const{userId, index}= req.body;
+
+    let user = await UserModel.findById(userId).populate("cart.product").populate("wishlist.product");
+   if (user) {
+    user.wishlist.splice(index,1);
+   }
+    user = await user.save();
+    res.status(200).json(user)
+
+  } catch (e) {
+      res.status(500).json({ Error: e.message })
+  }
+}
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { addToCart, addToWishlist } from '../controller/cartController.js';
+import { addToCart, addToWishlist, deletecartItem, deleteWishlisttItem, removecartItem } from '../controller/cartController.js';
 
 const router = express.Router();
 
 router.route('/cart').post(addToCart)
 router.route('/wishlist').post(addToWishlist)
+router.route('/removeCartItem').post(removecartItem)
+router.route('/deleteCartItem').post(deletecartItem)
+router.route('/deleteWishlistItem').post(deleteWishlisttItem)
+
 
 export default router
